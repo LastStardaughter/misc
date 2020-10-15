69f8c0ca5e68b6a1a644a397aaafe81b9200b211
Scottbert85 <58566722+Scottbert85@users.noreply.github.com>
2020-10-15 12:04:37 -0400

    Add files via upload

---
+++ ransomNote.java
@@ -0,0 +1,29 @@
+//coded together by Scott and jmsaylor
+    static void checkMagazine(String[] magazine, String[] note) {
+        boolean result = true;
+        Map<String, Integer> magazineMap = new HashMap();
+        Map<String, Integer> noteMap = new HashMap();
+        for (String word : magazine) {
+            if (magazineMap.putIfAbsent(word, 1) != null){
+                int temp = magazineMap.get(word);
+                temp++;
+                magazineMap.put(word, temp);
+            }
+        }
+        for (String word : note) {
+            if (noteMap.putIfAbsent(word, 1) != null){
+                int temp = noteMap.get(word);
+                temp++;
+                noteMap.put(word, temp);
+            }
+        }
+        for (String word : noteMap.keySet()) {
+            // System.out.println(magazineMap.get(word) + " : " + noteMap.get(word));
+            if (magazineMap.get(word) == null || noteMap.get(word) > magazineMap.get(word)){
+                result = false;
+            }
+        }
+        System.out.println( result ? "Yes" : "No");
+//        System.out.println(magazineMap);
+//        System.out.println(noteMap);
+    }
